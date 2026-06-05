c3f425161d20413641c65bba52b2bc42919e9ce9
fellowchrononaut <shreyankjois@gmail.com>
2026-06-05 15:57:52 +0200

    feat: Add Docker Compose configuration for sad_vio service and update run script

--- docker/run.sh
+++ docker/run.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-# Build the dockerfile 
-if [ -z "$(docker images -q sad_vio)" ]; then
-  docker build -f Dockerfile . -t sad_vio;
-fi
+# Build the dockerfile
+docker compose build
 
 # Allow X server connection
 xhost +local:root
-docker run -it \
-    --env="DISPLAY" \
-    --env="QT_X11_NO_MITSHM=1" \
-    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    sad_vio
+docker compose run sad_vio
 # Disallow X server connection
 xhost -local:root
